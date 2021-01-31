ab3a6f3e8dcab093af242116fa3eacca803fe8fd
tiketdatajosiaranda <josi.aranda@tiket.com>
2021-02-01 01:46:10 +0700

    add port env

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/josiaranda/cendana-residence-cibubur/handler"
 	"github.com/josiaranda/cendana-residence-cibubur/handler/User"
 	"github.com/josiaranda/cendana-residence-cibubur/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 func main() {
@@ -26,7 +28,11 @@ func main() {
 		Fb: app,
 	}
 
-	User.Register(handler,"api/users")
+	User.Register(handler, "api/users")
+	p := os.Getenv("PORT")
+	if p == "" {
+		p = "3000"
+	}
 
-	app.Listen(":3000")
+	app.Listen(fmt.Sprintf(":%s", p))
 }
