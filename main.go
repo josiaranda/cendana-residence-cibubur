2f21d419c82ee8ba873a42f8420f2b818033e1a3
tiketdatajosiaranda <josi.aranda@tiket.com>
2021-02-01 02:31:09 +0700

    change to env

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 
-	dsn := "host=ec2-54-237-135-248.compute-1.amazonaws.com user=vfzkoexcnqltui password=eec90a304424b821b23c662af6d1f178bb7fe6e1aea1354f0be3d28d8bbf63c0 dbname=dahssf7e60a7uf port=5432  TimeZone=Asia/Jakarta"
+	dsn := os.Getenv("DB_CONFIG")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
